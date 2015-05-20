2d04079415d4d59a8332532edbab980fb38a5131
Le Anh Tung <tung@laptrinh.com.vn>
2015-05-20 14:33:28 +0700

    Update IML Total Images: - Update code for generate image thumbnail with type

--- IML Total Images/IML Total Images/GenerateThumbnailForURL.m
+++ IML Total Images/IML Total Images/GenerateThumbnailForURL.m
@@ -1,6 +1,12 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSArray.h>
+#import <Foundation/NSURL.h>
+
+/// Comment this line if you don't want the type displayed inside the icon
+#define kNyxDisplayTypeInIcon
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
@@ -13,8 +19,40 @@ void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbn
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
-    // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
-    return noErr;
+    @autoreleasepool
+    {
+        CFDictionaryRef properties = NULL;
+#ifdef kNyxDisplayTypeInIcon
+        // Get the UTI properties
+        NSDictionary* utiDeclarations = (__bridge_transfer NSDictionary*)UTTypeCopyDeclaration(contentTypeUTI);
+        
+        // Get the extensions corresponding to the image UTI, for some UTI there can be more than 1 extension (ex image.jpeg = jpeg, jpg...)
+        id extensions = utiDeclarations[(__bridge NSString*)kUTTypeTagSpecificationKey][(__bridge NSString*)kUTTagClassFilenameExtension];
+        NSString* extension = ([extensions isKindOfClass:[NSArray class]]) ? extensions[0] : extensions;
+        
+        // Create the properties dic
+        CFTypeRef keys[1] = {kQLThumbnailPropertyExtensionKey};
+        CFTypeRef values[1] = {(__bridge CFStringRef)extension};
+        properties = CFDictionaryCreate(kCFAllocatorDefault, (const void**)keys, (const void**)values, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+#endif /* kNyxDisplayTypeInIcon */
+        // Check if the image is a PNG
+        if (CFStringCompare(contentTypeUTI, kUTTypePNG, kCFCompareCaseInsensitive) == kCFCompareEqualTo)
+        {
+
+            QLThumbnailRequestSetImageAtURL(thumbnail, url, properties);
+
+        }
+        else
+            QLThumbnailRequestSetImageAtURL(thumbnail, url, properties);
+        
+#ifdef kNyxDisplayTypeInIcon
+        if (properties != NULL)
+            CFRelease(properties);
+#endif /* kNyxDisplayTypeInIcon */
+        
+        return kQLReturnNoError;
+    }
+    
 }
 
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail)
